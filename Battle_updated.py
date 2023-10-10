42ede5c69ac2b60f8350a4c31d74c0e8fab1d541
Waleed-P <147254624+Waleed-P@users.noreply.github.com>
2023-10-10 08:24:05 +0530

    Add files via upload

---
+++ Battle_updated.py
@@ -0,0 +1,44 @@
+import random
+Player_hp=100
+Dragon_hp=100
+def generate_random_story():
+    players = ["Hero", "Adventurer", "Knight", "Warrior", "Explorer"]
+    actions = ["encountered", "fought", "battled", "confronted", "challenged"]
+    dragons = ["Dragon", "Wyvern", "Drake", "Serpent", "Firedrake"]
+    outcomes = ["victorious", "triumphant", "defeated", "brave", "courageous"]
+    locations = ["mountain", "cave", "forest", "valley", "castle"]
+    player = random.choice(players)
+    action = random.choice(actions)
+    dragon = random.choice(dragons)
+    outcome = random.choice(outcomes)
+    location = random.choice(locations)
+    print("Once upon a time..",player,action,'a mighty ',dragon,'in the',location,'and emerged ',outcome,'!')
+
+generate_random_story()
+for i in range(0,20):
+    dhp=random.randint(1,20)
+    php=random.randint(1,20)
+    
+    if Dragon_hp<=0 or Player_hp<0:
+        break
+    
+    Dragon_hp=Dragon_hp-dhp
+    Player_hp=Player_hp-php
+    print('player hp=',Player_hp,'dragon hp=',Dragon_hp)
+    
+if Dragon_hp<=0 and Player_hp>=0:
+    print("player wins")
+elif Player_hp<=0 and Dragon_hp>=0:
+    print("Dragon wins")
+elif Player_hp<=0 and Dragon_hp<0:
+    if Player_hp>Dragon_hp:
+        print("player wins")
+    else:
+        print("Dragon wins")
+elif Player_hp>0 and Dragon_hp>0:
+    if Player_hp>Dragon_hp:
+        print("Player wins")
+    else:
+        print("Dragons wins")
+elif Player_hp==0 and Dragon_hp==0:
+    print('Tie')
